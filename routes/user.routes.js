ff7058e8d81680e36dc7dcbf131e875754a9a0bf
technologicaljerry <cserajatverma@gmail.com>
2023-12-22 22:17:30 +0530

    Update user.routes.js

---
+++ routes/user.routes.js
@@ -0,0 +1,19 @@
+const usersData = require('../dummyDatabase.json');
+
+function userRoutes(fastify, options, done) {
+
+    fastify.get('/healthcheck', (req, res) => {
+        res.send({
+            message: 'Hello, Welcome to Fastify Server.'
+        })
+    })
+
+    fastify.get('/getAllUsers', (req, res) => {
+        res.send(usersData)
+    })
+
+    done()
+
+}
+
+module.exports = userRoutes
