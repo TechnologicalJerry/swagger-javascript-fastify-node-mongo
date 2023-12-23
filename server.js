03526808e44f9e585136b80e96f024903570b387
technologicaljerry <cserajatverma@gmail.com>
2023-12-23 22:12:32 +0530

    Update server.js

--- server.js
+++ server.js
@@ -7,6 +7,7 @@ const PORT = 5050;
 const start = async () => {
     try {
         await fastify.listen(PORT);
+        console.log(`SERVER Running on HOST:${HOST} PORT:${PORT}`);
     } catch (error) {
         fastify.log.error(error);
         process.exit(1);
