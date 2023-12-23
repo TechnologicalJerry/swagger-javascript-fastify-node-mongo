6e86247f94eb476461c294c76319ed1c115ff72d
technologicaljerry <cserajatverma@gmail.com>
2023-12-23 22:32:14 +0530

    Update user.routes.js

--- routes/user.routes.js
+++ routes/user.routes.js
@@ -8,7 +8,7 @@ function userRoutes(fastify, options, done) {
         })
     })
 
-    fastify.get('/getAllUsers', (req, res) => {
+    fastify.get('/getAllUsers', getUsersOptions, (req, res) => {
         res.send(usersData)
     })
 
